49f7ba20aa86691b1334b8e424cd24bf5f42ecfd
juyonLee00 <ljy012900@gmail.com>
2025-12-03 23:56:07 +0900

    solve : 2025-12-03 문제 해결

---
+++ 2025/20251203.sql
@@ -0,0 +1,13 @@
+-- 문제 링크 : https://solvesql.com/problems/species-and-mass-of-penguins/
+
+SELECT
+  species,
+  body_mass_g
+FROM
+  penguins
+WHERE
+  species IS NOT NULL
+  AND body_mass_g IS NOT NULL
+ORDER BY
+  body_mass_g DESC,
+  species ASC

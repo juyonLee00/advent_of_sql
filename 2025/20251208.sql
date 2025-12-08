08fdbfcff7d5817de95f12578a76376459050e16
juyonLee00 <ljy012900@gmail.com>
2025-12-08 22:43:56 +0900

    solve : 2025-12-08 문제 해결

---
+++ 2025/20251208.sql
@@ -0,0 +1,37 @@
+-- 문제 : https://solvesql.com/problems/wines-for-friends/
+
+SELECT
+  *
+FROM
+  wines
+WHERE
+  color = "white"
+  AND quality >= 7
+  AND density > (
+    SELECT
+      AVG(density)
+    FROM
+      wines
+  )
+  AND residual_sugar > (
+    SELECT
+      AVG(residual_sugar)
+    FROM
+      wines
+  )
+  AND pH < (
+    SELECT
+      AVG(pH)
+    FROM
+      wines
+    WHERE
+      color = "white"
+  )
+  AND citric_acid > (
+    SELECT
+      AVG(citric_acid)
+    FROM
+      wines
+    WHERE
+      color = "white"
+  )
